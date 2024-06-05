740eb16bf9b9a4819635c52258752367d75f19ec
EstebanDuque123 <61398090+EstebanDuque123@users.noreply.github.com>
2024-06-05 00:25:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if st.button("Predecir"):
     }
     print("Seabrio la interface")
     # Realizamos la solicitud al endpoint de FastAPI
-    response = requests.post("https://predicci-n-de-modalidad-de-robo-de-1wjt.onrender.com", json=datos_entrada)
+    response = requests.post("https://predicci-n-de-modalidad-de-robo-de.onrender.com", json=datos_entrada)
     
     # Verificamos la respuesta
     if response.status_code == 200:
